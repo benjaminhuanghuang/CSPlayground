8e79e655fd553b3b07fa03889f311e80bcfc9ccc
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-01-18 10:48:54 -0800

    update

--- Program.cs
+++ Program.cs
@@ -16,9 +16,13 @@ namespace CSPlayground
             // Console.ReadLine(); // Wait the async function.
             
             // -- Thread
-            MultiThreadDemo.TwoThreads();
-
-            
+            // MultiThreadDemo.TwoThreads();
+            double a = 2.0;
+            Console.WriteLine("{0:F}",a);
+            Console.WriteLine("{0:F1}",a);
+            Console.WriteLine("{0,5:F1}",a);  // arg0, right align, float
+            Console.WriteLine("{0,-5:F1}",a); // arg0, left align, float
+            // Console.WriteLine("{-10:F1}",a);
         }
     }
 }
